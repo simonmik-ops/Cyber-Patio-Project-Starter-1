39932e13f090a1e32f73bbed7e82af1f61d08b61
John Pett <3016847+JohnPett@users.noreply.github.com>
2023-01-09 22:10:44 +0700

    Add tools

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log('Hello, world!');
+function init() {
+  console.log(document.querySelector("h1"));
+}
+
+if (document.addEventListener) document.addEventListener("DOMContentLoaded", init);
+else window.attachEvent("onload", init);
