c60506cc3c3785765dab79bcbc61e34470631cc1
John Pett <3016847+JohnPett@users.noreply.github.com>
2023-01-21 18:52:02 +0700

    Add comments

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-function onImageClick(event) {
-  const dialog = document.querySelector('dialog');
-  const src = event.currentTarget.getAttribute('src');
-  const img = new Image();
+function onImageClick(event) { // On each image clicked give me the corresponding event
+  const dialog = document.querySelector('dialog'); // Target location for the HTML we are creating with JS
+  const src = event.currentTarget.getAttribute('src'); // Grabbing the source of the image clicked
+  const img = new Image(); // Create HTML <img> tag with JS
 
-  img.onload = function() {
-    dialog.innerHTML = `<img src="${img.src}" />`; 
-    dialog.style.display = 'flex';
+  img.onload = function() { // Once my image has loaded...
+    dialog.innerHTML = `<img src="${img.src}" />`;  // Go find the <dialog> previously defined above
+    dialog.style.display = 'flex'; // Show the hidden dialog box
   };
 
-  img.src = src;
+  img.src = src; // Define the src attribute of the <img src="_image_source_" />
 }
 
 function addEventListeners() {
-  const images = [...document.querySelectorAll('img')];
-  images.forEach(image => image.addEventListener('click', onImageClick));
+  const images = [...document.querySelectorAll('img')]; // Query the whole document for imgs, then put them in an array
+  images.forEach(image => image.addEventListener('click', onImageClick)); // For each image, create a unique 'click' event
 }
 
 function init() {
-  addEventListeners();
+  addEventListeners(); // I'm loaded, now add events to HTML elements
 }
 
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("DOMContentLoaded", init); // Once all HTML has loaded, fire an event called init()
