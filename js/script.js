f8912c89e14a7aad98b4e835042189c4c5438de6
John Pett <3016847+JohnPett@users.noreply.github.com>
2023-01-21 18:43:05 +0700

    Basic dialog

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
+function onImageClick(event) {
+  const dialog = document.querySelector('dialog');
+  const src = event.currentTarget.getAttribute('src');
+  const img = new Image();
+
+  img.onload = function() {
+    dialog.innerHTML = `<img src="${img.src}" />`; 
+    dialog.style.display = 'flex';
+  };
+
+  img.src = src;
+}
+
+function addEventListeners() {
+  const images = [...document.querySelectorAll('img')];
+  images.forEach(image => image.addEventListener('click', onImageClick));
+}
+
 function init() {
-  console.log(document.querySelector("h1"));
+  addEventListeners();
 }
 
 document.addEventListener("DOMContentLoaded", init);
