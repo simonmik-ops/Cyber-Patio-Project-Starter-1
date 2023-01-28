c29339455a7c16582a5187b6f85061340bf3891c
victoiredarasse <122304607+victoiredarasse@users.noreply.github.com>
2023-01-28 10:24:25 +0100

    WEBSITE

--- js/script.js
+++ js/script.js
@@ -1,23 +1,18 @@
-function onImageClick(event) { // On each image clicked give me the corresponding event
-  const dialog = document.querySelector('dialog'); // Target location for the HTML we are creating with JS
-  const src = event.currentTarget.getAttribute('src'); // Grabbing the source of the image clicked
-  const img = new Image(); // Create HTML <img> tag with JS
-
-  img.onload = function() { // Once my image has loaded...
-    dialog.innerHTML = `<img src="${img.src}" />`;  // Go find the <dialog> previously defined above
-    dialog.style.display = 'flex'; // Show the hidden dialog box
+function init() {
+  function onNumberHover(event) {
+    const src = event.currentTarget.getAttribute('data-src');
+    const background = document.querySelector('.background');
+    const img = new Image();
+    img.onload = function() {
+      background.innerHTML = `<img src="${img.src}" />`;
+    }
+    img.src = src;
   };
 
-  img.src = src; // Define the src attribute of the <img src="_image_source_" />
+  const numbers = [...document.querySelectorAll('.content__number')];
+  numbers.forEach(function(number) {
+    number.addEventListener('mouseover', onNumberHover);
+  });
 }
 
-function addEventListeners() {
-  const images = [...document.querySelectorAll('img')]; // Query the whole document for imgs, then put them in an array
-  images.forEach(image => image.addEventListener('click', onImageClick)); // For each image, create a unique 'click' event
-}
-
-function init() {
-  addEventListeners(); // I'm loaded, now add events to HTML elements
-}
-
-document.addEventListener("DOMContentLoaded", init); // Once all HTML has loaded, fire an event called init()
+document.addEventListener("DOMContentLoaded", init);
